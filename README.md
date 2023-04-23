00fa12d40fb2d1039f1664dcbe49e9c34ceae1ec
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-23 13:33:46 -0700

    learrning collaboration

---
+++ README.md
@@ -0,0 +1,1 @@
+Using this to learn git pull, push, merge
---
+++ basic-code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*our main function*/
+/**
+ * main - Our main function
+ *
+ * Return: returns int
+ */
+int main(void)
+{
+	int i = 2;
+	int j = 4;
+
+	printf("my sum %d\n", i + j);
+}
